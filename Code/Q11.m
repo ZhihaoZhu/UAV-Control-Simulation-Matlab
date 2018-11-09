9d14dfc88bc01b7bf3bbb1a30678fa5048a1df90
Steve Zhu <704242527zzh@gmail.com>
2018-11-08 23:04:48 -0500

    add

--- Code/Q11.m
+++ Code/Q11.m
@@ -1,6 +1,10 @@
+clear;
+clc;
+close all;
+
 % Simulation times, in seconds. 
 start_time = 0; 
-end_time = 0.6; 
+end_time = 10; 
 dt = 0.01; 
 times = start_time:dt:end_time;
 N = numel(times);
@@ -83,82 +87,144 @@ for iter = 1:N-2
 
     free_skate(iter,time,end_time);
     
-    % generate the Force and Torque
-    [F_des, M_des] = controller(iter, ctrl, plant_params); % generate force, torque
-    [F_act,M_act] = motor_model(iter, dt, ctrl, plant_params, F_des, M_des);
-    
-    % generate state vector
-    s = state_vector(iter);
-
-    % generate the derivative of state vector "s"
-    sdot = physical_model(iter, F_act, M_act,s,plant_params); 
-
-    % update state
-    update_state(iter,sdot,dt);
+    update_state(iter,ctrl,plant_params, dt);
 
     time = time + dt;
 end
 
 %% ************************ plot the trajectory *********************
-disp('Initializing figures...');
-
 x = state.pos(1,:);
 y = state.pos(2,:);
 z = state.pos(3,:);
-
+start_point = 1;
+end_point = N-2;
 figure(1)
 subplot(1,3,1);
-plot(x);
+plot(x(start_point:end_point),'linewidth',1);
+ylim([-1 1])
 hold on
+plot(des_state.pos(1,start_point:end_point),'linewidth',1);
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m)','FontSize',10);
+legend('Actual X Position','Desire X Position');
+hold on
+title("x")
 subplot(1,3,2);
-plot(y);
+plot(y(start_point:end_point), 'linewidth',1);
+
 hold on
+plot(des_state.pos(2,start_point:end_point),'linewidth',1);
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m)','FontSize',10);
+legend('Actual Y Position','Desire Y Position');
+hold on
+title("y")
 subplot(1,3,3);
-plot(z);
+plot(z(start_point:end_point), 'linewidth',1);
 hold on
+plot(des_state.pos(3,start_point:end_point),'linewidth',1);
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m)','FontSize',10);
+legend('Actual Z Position','Desire Z Position');
+hold on
+title("z")
 
 
-figure(2)
+figure('Name', "error_pos")
 subplot(1,3,1);
-plot(des_state.pos(1,:));
+plot(des_state.pos(1,start_point:end_point)-state.pos(1,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m)','FontSize',10);
 hold on
+title("error_x")
 subplot(1,3,2);
-plot(des_state.pos(2,:));
+plot(des_state.pos(2,start_point:end_point)-state.pos(2,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m)','FontSize',10);
 hold on
+title("error_y")
 subplot(1,3,3);
-plot(des_state.pos(3,:));
+plot(des_state.pos(3,start_point:end_point)-state.pos(3,start_point:end_point),'linewidth',1);
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m)','FontSize',10);
 hold on
+title("error_z")
 
-% figure(3)
-% subplot(1,3,1);
-% plot(des_state.vel(1,:));
-% hold on
-% subplot(1,3,2);
-% plot(des_state.vel(2,:));
-% hold on
-% subplot(1,3,3);
-% plot(des_state.vel(3,:));
-% hold on
 
-figure(4)
+figure('Name', "error_vel")
 subplot(1,3,1);
-plot(state.omega(1,:));
+plot(des_state.vel(1,start_point:end_point)-state.vel(1,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m/s)','FontSize',10);
 hold on
+title("error xvel")
 subplot(1,3,2);
-plot(state.omega(2,:));
+plot(des_state.vel(2,start_point:end_point)-state.vel(2,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m/s)','FontSize',10);
 hold on
+title("error yvel")
 subplot(1,3,3);
-plot(state.omega(3,:));
+plot(des_state.vel(3,start_point:end_point)-state.vel(3,start_point:end_point),'linewidth',1);
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(m/s)','FontSize',10);
 hold on
+title("error zvel")
 
-% figure(5)
-% subplot(1,3,1);
-% plot(x-des_state.pos(1,:));
-% hold on
-% subplot(1,3,2);
-% plot(y-des_state.pos(2,:));
-% hold on
-% subplot(1,3,3);
-% plot(z-des_state.pos(3,:));
-% hold on
 
+figure('Name', "error_rot")
+subplot(1,3,1);
+plot(des_state.rot(1,start_point:end_point)-state.rot(1,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad)','FontSize',10);
+hold on
+title("error phi")
+subplot(1,3,2);
+plot(des_state.rot(2,start_point:end_point)-state.rot(2,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad)','FontSize',10);
+hold on
+title("error theta")
+subplot(1,3,3);
+plot(des_state.rot(3,start_point:end_point)-state.rot(3,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad)','FontSize',10);
+hold on
+title("error yaw")
+
+figure('Name', "error_omega")
+subplot(1,3,1);
+plot(des_state.omega(1,start_point:end_point)-state.omega(1,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad/s)','FontSize',10);
+hold on
+title("error phi vel")
+subplot(1,3,2);
+plot(des_state.omega(2,start_point:end_point)-state.omega(2,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad/s)','FontSize',10);
+hold on
+title("error theta vel")
+subplot(1,3,3);
+plot(des_state.omega(3,start_point:end_point)-state.omega(3,start_point:end_point),'linewidth',1);
+ylim([-1 1])
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad/s)','FontSize',10);
+hold on
+title("error yaw vel")
+
+figure(5)
+plot(des_state.omega(3,start_point:end_point));
+xlabel('(0.01 s)','FontSize',10);
+ylabel('(rad/s)','FontSize',10);
+hold on
+title(" yawvel")
